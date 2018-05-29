f789242c373c766bb89bf935ea665766f9135e6f
Lawrence Yoon <yoonlawrence@yahoo.com>
2018-05-28 20:36:33 -0700

    Added three reveal on scrolls - info-box, title and textbox in about section.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".info-box"), "85%");
+new RevealOnScroll($(".about .title"), "60%");
+new RevealOnScroll($(".about .textbox"), "60%");
