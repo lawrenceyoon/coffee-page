c0e524690c4aee3f5a88361923b606affccb0a8d
Lawrence Yoon <yoonlawrence@yahoo.com>
2018-05-29 19:02:49 -0700

    Finished smooth scrolling, and changes color depending on section you're on.

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,11 +1,20 @@
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
   constructor() {
     this.Header = $(".header");
     this.headerTriggerElement = $(".large-hero h1");
     this.createHeaderWaypoint();
+    this.pageSections = $(".page-section");
+    this.headerLinks = $(".header a");
+    this.createPageSectionWaypoints();
+    this.addSmoothScrolling();
+  }
+
+  addSmoothScrolling() {
+    this.headerLinks.smoothScroll();
   }
 
   createHeaderWaypoint() {
@@ -17,8 +26,39 @@ class StickyHeader {
           that.Header.addClass("header-dark");
         } else {
           that.Header.removeClass("header-dark");
+          that.headerLinks.removeClass("is-current-link");
         }
       }
+    });
+  }
+
+  createPageSectionWaypoints() {
+    var that = this;
+    this.pageSections.each(function() {
+      var currentPageSection = this;
+      new Waypoint({
+        element: currentPageSection,
+        handler: function(direction) {
+          if(direction == "down") {
+            var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+            that.headerLinks.removeClass("is-current-link");
+            $(matchingHeaderLink).addClass("is-current-link");
+          }
+        },
+        offset: "18%"
+      });
+
+      new Waypoint({
+        element: currentPageSection,
+        handler: function(direction) {
+          if(direction == "up") {
+            var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+            that.headerLinks.removeClass("is-current-link");
+            $(matchingHeaderLink).addClass("is-current-link");
+          }
+        },
+        offset: "-40%"
+      });
     });
   }
 }
