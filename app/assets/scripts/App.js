83269ffbb686ccd8939a7c2090c79103e6db957f
Lawrence Yoon <yoonlawrence@yahoo.com>
2018-06-01 10:37:00 -0700

    Finished modal section, next working on faster page loading.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,9 +2,11 @@ import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".info-box"), "85%");
 new RevealOnScroll($(".about .title"), "60%");
 new RevealOnScroll($(".about .textbox"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
